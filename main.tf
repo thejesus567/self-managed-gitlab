4a739e6aa7f94baa427de2bfcf6993170f001815
Jesús Iván Lemus Cervantes <thejesus567@gmail.com>
2025-08-01 00:15:32 -0600

    added user data script

--- main.tf
+++ main.tf
@@ -13,6 +13,9 @@ module "vpc" {
   enable_vpn_gateway = false
 
   manage_default_security_group = false
+  map_public_ip_on_launch = true
+  enable_dns_support = true
+  enable_dns_hostnames = true
 
   tags = {
     Terraform   = "true"
@@ -26,7 +29,7 @@ resource "aws_security_group" "gitlab-server-sg" {
   description = "security group for gitlab server"
   vpc_id      = module.vpc.vpc_id
   tags = {
-    Name = "example"
+    Name = "gitlab-server-sg"
   }
 }
 
@@ -46,6 +49,15 @@ resource "aws_vpc_security_group_ingress_rule" "http-ingress-rule" {
   from_port   = 80
   ip_protocol = "tcp"
   to_port     = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "https-ingress-rule" {
+  security_group_id = aws_security_group.gitlab-server-sg.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 443
+  ip_protocol = "tcp"
+  to_port     = 443
 }
 
 resource "aws_vpc_security_group_egress_rule" "example" {
@@ -75,7 +87,7 @@ data "aws_ami" "amzn-linux-2023-ami" {
     name   = "name"
     values = ["al2023-ami-2023.*-x86_64"]
   }
-}
+} 
 
 //server
 resource "aws_instance" "gitlab-server" {
@@ -85,6 +97,31 @@ resource "aws_instance" "gitlab-server" {
   vpc_security_group_ids      = [aws_security_group.gitlab-server-sg.id]
   associate_public_ip_address = true
   key_name                    = aws_key_pair.gitlab-key.key_name
+
+    user_data = <<-EOF
+              #!/bin/bash
+
+              # Enable SSH
+              sudo systemctl enable --now sshd
+
+              # Install and enable firewalld
+              sudo dnf install -y firewalld
+              sudo systemctl enable --now firewalld
+
+              # Open necessary ports
+              sudo firewall-cmd --permanent --add-service=ssh
+              sudo firewall-cmd --permanent --add-service=http
+              sudo firewall-cmd --permanent --add-service=https
+              sudo systemctl reload firewalld
+
+              # Install GitLab CE repo
+              sleep 20
+              curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+
+              # Get public IP and install GitLab
+              PUBLIC_IP=$(curl -s ifconfig.me)
+              sudo EXTERNAL_URL="http://$PUBLIC_IP" dnf install -y gitlab-ce
+              EOF
 
   tags = {
     Name = "gitlab-server"
