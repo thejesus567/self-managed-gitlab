f4718ec8b5ec5484c39fc8abfdb3725bbe602fd5
Jesus Ivan Lemus Cervantes <thejesus567@gmail.com>
2026-01-30 21:09:10 -0600

    updated iam role name (using locals)

--- s3.tf
+++ s3.tf
@@ -48,7 +48,7 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
 
 
 resource "aws_iam_role" "instance" {
-  name               = "instance_role"
+  name               = local.role_name
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
 
   inline_policy {
