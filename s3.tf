2a77e564de1707c9ab8a22b2291175d480a8667c
Jesus Ivan Lemus Cervantes <thejesus567@gmail.com>
2026-02-01 22:15:04 -0600

    refactor: iam policy by data document

--- s3.tf
+++ s3.tf
@@ -3,37 +3,35 @@ locals {
   role_name   = "GitLabS3Access"
 }
 
+data "aws_iam_policy_document" "s3-policy-document" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:PutObjectAcl"
+    ]
+    resources = ["arn:aws:s3:::gl-*/*"]
+    effect    = "Allow"
+  }
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:AbortMultipartUpload",
+      "s3:ListMultipartUploadParts",
+      "s3:ListBucketMultipartUploads"
+    ]
+    resources = ["arn:aws:s3:::gl-*"]
+    effect    = "Allow"
+  }
+}
+
 resource "aws_iam_policy" "s3-policy" {
   name        = local.policy_name
   description = "IAM Polcicy for gl :aws_s3_buckets3 buckets"
+  policy      = data.aws_iam_policy_document.s3-policy-document
+}
 
-  policy = <<EOT
-{   "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:PutObject",
-                "s3:GetObject",
-                "s3:DeleteObject",
-                "s3:PutObjectAcl"
-            ],
-            "Resource": "arn:aws:s3:::gl-*/*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "s3:ListBucket",
-                "s3:AbortMultipartUpload",
-                "s3:ListMultipartUploadParts",
-                "s3:ListBucketMultipartUploads"
-            ],
-            "Resource": "arn:aws:s3:::gl-*"
-        }
-    ]
-}
-EOT
-}
 
 data "aws_iam_policy_document" "instance_assume_role_policy" {
   statement {
