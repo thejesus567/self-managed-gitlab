f74272290f0ad3bea9687d30283a3f80cf5cea50
Jesus Ivan Lemus Cervantes <thejesus567@gmail.com>
2026-02-04 18:46:43 -0600

    renamed files

---
+++ vpc.tf
@@ -0,0 +1,27 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "gitlab-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.0.0.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.3.0/24"]
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = false
+  one_nat_gateway_per_az = true
+
+  map_public_ip_on_launch = true
+
+  # Uncomment the line below if you require dedicated hardware.
+  # instance_tenancy = "dedicated"
+
+  tags = {
+    Terraform   = "true"
+    Environment = var.env
+  }
+
+  # IGW tag overrides
+  igw_tags = var.igw_tags
+}
