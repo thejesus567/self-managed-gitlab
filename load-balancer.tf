99b047fcd947686e5f66f6383f19d38ab07bdcd4
Jesus Ivan Lemus Cervantes <thejesus567@gmail.com>
2026-02-06 16:22:07 -0600

    refactor: network load balancer
    removed unnecessary stuff from the load balancer. I am gonna handle
    listeners and targets in different reources

---
+++ load-balancer.tf
@@ -0,0 +1,53 @@
+## NLB Security group 
+
+resource "aws_security_group" "lb-sg" {
+  name        = "gitlab-loadbalancer-sec-group"
+  description = "Security group for Network Load Balancer"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Environment = var.env
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.lb-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.lb-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.lb-sg.id
+  cidr_ipv4         = "0.0.0.0/0" #TODO: Add a single trusted IP address, or an IP address range in CIDR notation.
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+module "nlb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name               = "gitlab-loadbalancer"
+  load_balancer_type = "network"
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+
+  # enforce_security_group_inbound_rules_on_private_link_traffic = "on"
+
+  # Security Group
+  security_groups = [aws_security_group.lb-sg.id]
+
+  tags = {
+    Environment = var.env
+  }
+}
