9c87826c7297f44e0e7e00b1666d0c01617d8c91
Jesus Ivan Lemus Cervantes <thejesus567@gmail.com>
2026-02-09 23:18:25 -0600

    added https listener
    This version fails in apply stage (400). I still need to create a
    certificate

--- load-balancer.tf
+++ load-balancer.tf
@@ -100,4 +100,21 @@ resource "aws_lb_listener" "ssh" {
   }
 }
 
-#TODO: Add https listener
+#TODO: Create a certificate 
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = module.nlb.arn
+  port              = "443"
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  # certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.https-target.arn
+  }
+}
+
+
+
+
