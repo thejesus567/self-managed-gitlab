d07576246e51d6a1e8b4ed4385dec7177a26531e
Jesus Ivan Lemus Cervantes <thejesus567@gmail.com>
2026-02-16 20:47:58 -0600

    added last things for rds data base

--- rds.tf
+++ rds.tf
@@ -28,10 +28,10 @@ resource "aws_vpc_security_group_ingress_rule" "postgresql-ingress" {
 }
 
 # DB Subnet group
+# TODO: Check if this is actually nedeed
 resource "aws_db_subnet_group" "rds-subnet-group" {
   name       = local.db-subnet-group
   subnet_ids = module.vpc.private_subnets
-
   tags = {
     Name = local.db-subnet-group
   }
@@ -53,62 +53,19 @@ module "db" {
   port     = local.db-port
 
   #TODO: Check thing below
-  iam_database_authentication_enabled = true
+  #iam_database_authentication_enabled = true
 
-  vpc_security_group_ids = ["sg-12345678"]
+  vpc_security_group_ids = [aws_security_group.rds-sg.id]
 
-  maintenance_window = "Mon:00:00-Mon:03:00"
-  backup_window      = "03:00-06:00"
-
-  # Enhanced Monitoring - see example for details on how to create the role
-  # by yourself, in case you don't want to create it automatically
-  monitoring_interval    = "30"
-  monitoring_role_name   = "MyRDSMonitoringRole"
-  create_monitoring_role = true
+  #TODO: Check if this would be good
+  create_db_parameter_group = false
 
   tags = {
-    Owner       = "user"
-    Environment = "dev"
+    Environment = var.env
   }
 
   # DB subnet group
   create_db_subnet_group = true
-  subnet_ids             = ["subnet-12345678", "subnet-87654321"]
+  subnet_ids             = module.vpc.private_subnets
 
-  # DB parameter group
-  family = "mysql8.0"
-
-  # DB option group
-  major_engine_version = "8.0"
-
-  # Database Deletion Protection
-  deletion_protection = true
-
-  parameters = [
-    {
-      name  = "character_set_client"
-      value = "utf8mb4"
-    },
-    {
-      name  = "character_set_server"
-      value = "utf8mb4"
-    }
-  ]
-
-  options = [
-    {
-      option_name = "MARIADB_AUDIT_PLUGIN"
-
-      option_settings = [
-        {
-          name  = "SERVER_AUDIT_EVENTS"
-          value = "CONNECT"
-        },
-        {
-          name  = "SERVER_AUDIT_FILE_ROTATIONS"
-          value = "37"
-        },
-      ]
-    },
-  ]
 }
