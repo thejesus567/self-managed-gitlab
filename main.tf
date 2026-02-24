63a63aab296c02be4af6354ffaf868fe3ebd42b7
Jesus Ivan Lemus Cervantes <thejesus567@gmail.com>
2026-02-23 21:58:25 -0600

    defined versions

--- main.tf
+++ main.tf
@@ -4,7 +4,12 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.2.0"
+    }
   }
+  required_version = "~> 1.14.0"
 }
 
 # Configure the AWS Provider
