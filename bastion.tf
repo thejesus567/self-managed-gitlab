c52cf6c0c90a7a2e4d9de2044f9f3f6729df3a0e
Jesus Ivan Lemus Cervantes <thejesus567@gmail.com>
2026-02-25 22:20:26 -0600

    added security group for bastion host

--- bastion.tf
+++ bastion.tf
@@ -1,6 +1,26 @@
 locals {
-  ec2-name = "Bastion Host A"
+  ec2-name        = "Bastion Host A"
+  bastion-sg-name = "bastion-sec-group"
 
+}
+
+resource "aws_security_group" "bastion-sg" {
+  name        = local.lb-sg-name
+  description = "Security group for Bastion host"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name        = local.bastion-sg-name
+    Environment = var.env
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_bastion_ssh" {
+  security_group_id = aws_security_group.bastion-sg.id
+  cidr_ipv4         = "0.0.0.0/0" #TODO: Add a single trusted IP address, or an IP address range in CIDR notation.
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
 }
 
 data "aws_ami" "ubuntu" {
@@ -17,7 +37,6 @@ data "aws_ami" "ubuntu" {
 # in order to use ephemeral values for my key pair
 # https://github.com/hashicorp/terraform-provider-aws/pull/44102
 
-
 # RSA key of size 4096 bits
 #ephemeral "tls_private_key" "rsa" {
 #  algorithm = "RSA"
@@ -30,15 +49,23 @@ resource "aws_key_pair" "gitlab-key" {
   public_key = file("~/.ssh/gitlab-key.pub")
 
   tags = {
-    Name = "gitlab-key"
+    Name = "bastion-host-a"
   }
 
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "bastion-host-a" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
 
+  subnet_id = module.vpc.public_subnets[0]
+
+  #TODO: missing eni
+
+  vpc_security_group_ids = [aws_security_group.bastion-sg.id]
+
+
+  key_name = aws_key_pair.gitlab-key.key_name
   tags = {
     Name = local.ec2-name
   }
